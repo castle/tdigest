25c038e1646441fc9c791d8aa2fb7fca2e89f796
Lukas Fittl <lukas@fittl.com>
2015-11-21 21:27:13 -0800

    Fix edge case in percentile with little data.

--- test/tdigest_test.rb
+++ test/tdigest_test.rb
@@ -5,4 +5,11 @@ class TDigestTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TDigest::VERSION
   end
+
+  def test_percentile_edge_case
+    tdigest = ::TDigest::TDigest.new
+    tdigest.push(60, 100)
+    pct = tdigest.percentile(0.90) # This should not crash
+    assert_equal nil, pct
+  end
 end
